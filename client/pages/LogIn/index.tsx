e4f4c66c9c37d8cb9339cf9bfa48998279b3c893
jjhstoday <jjh.s.today@gmail.com>
2021-06-04 22:41:14 +0900

    feat: swr 사용한 상태 관리 및 쿠키 공유

--- client/pages/LogIn/index.tsx
+++ client/pages/LogIn/index.tsx
@@ -3,25 +3,34 @@ import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } fro
 import useInput from '@hooks/useInput';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
 
 export default function LogIn() {
+  // useSWR: 첫번째 매개변수인 url 주소값이 두번째 매개변수인 함수의 매개변수로 들어가서 반환값을 반환한다.
+  const { data, error, revalidate } = useSWR('http://localhost:3095/api/users', fetcher, {
+    dedupingInterval: 100000,
+  });
   const [logInError, setLogInError] = useState(false);
   const [email, onChangeEmail] = useInput('');
   const [password, onChangePassword] = useInput('');
 
+  // withCredentials: 쿠키 생성 → post에서는 세번째 매개변수로, get에서는 두번째 매개변수로
   const onSubmit = useCallback(
     (e) => {
       e.preventDefault();
       setLogInError(false);
       axios
         .post(
-          '/api/users/login',
+          'http://localhost:3095/api/users/login',
           { email, password },
           {
             withCredentials: true,
           },
         )
-        .then((response) => {})
+        .then((response) => {
+          revalidate();
+        })
         .catch((error) => {
           setLogInError(error.response?.data?.statusCode === 401);
         });
