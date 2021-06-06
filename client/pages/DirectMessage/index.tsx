93f6e49766f3f842e14deb6bae8735b2e9591ade
jjhstoday <jjh.s.today@gmail.com>
2021-06-07 02:01:45 +0900

    feat: 채팅 보내기 비동기 처리

--- client/pages/DirectMessage/index.tsx
+++ client/pages/DirectMessage/index.tsx
@@ -7,17 +7,37 @@ import fetcher from '@utils/fetcher';
 import ChatBox from '@components/ChatBox';
 import ChatList from '@components/ChatList';
 import useInput from '@hooks/useInput';
+import axios from 'axios';
+import { IDM } from '@typings/db';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSWR(`/api/users`, fetcher);
-  const [chat, onChangeChat] = useInput('');
+  const [chat, onChangeChat, setChat] = useInput('');
+  const {
+    data: chatData,
+    mutate: mutateChat,
+    revalidate,
+  } = useSWR<IDM[]>(`/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`, fetcher);
 
-  const onSubmitForm = useCallback((e) => {
-    e.preventDefault();
-    console.log('submit');
-  }, []);
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (chat?.trim()) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            revalidate();
+            setChat('');
+          })
+          .catch(console.error);
+      }
+    },
+    [chat],
+  );
 
   if (!userData || !myData) {
     return null;
