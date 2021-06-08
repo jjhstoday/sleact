a7a744e95ee7948c5733918013402b93a17cb99b
jjhstoday <jjh.s.today@gmail.com>
2021-06-08 23:45:44 +0900

    chore: 타입 점검 및 추가

--- client/hooks/useInput.ts
+++ client/hooks/useInput.ts
@@ -1,11 +1,11 @@
-import { useState, useCallback, SetStateAction, Dispatch } from 'react';
+import { useState, useCallback, SetStateAction, Dispatch, ChangeEvent } from 'react';
 
-type ReturnTypes<T = any> = [T, (e: any) => void, Dispatch<SetStateAction<T>>];
+type ReturnTypes<T> = [T, (e: ChangeEvent<HTMLInputElement>) => void, Dispatch<SetStateAction<T>>];
 
-const useInput = <T = any>(initialData: T): ReturnTypes<T> => {
+const useInput = <T>(initialData: T): ReturnTypes<T> => {
   const [value, setValue] = useState(initialData);
-  const handler = useCallback((e: any) => {
-    setValue(e.target.value);
+  const handler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value as unknown as T);
   }, []);
 
   return [value, handler, setValue];
